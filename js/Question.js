7ed1613c14f2f21dcaf3388b687db7afb008900a
Aditya4713 <73423962+Aditya4713@users.noreply.github.com>
2021-02-15 14:46:44 +0530

    Add files via upload

---
+++ js/Question.js
@@ -0,0 +1,24 @@
+class Question {
+
+    constructor() {
+      
+    }
+    hide(){
+      
+    }
+  
+    display(){
+      
+      this.title.html("My Quiz Game");
+      title.position(350, 0);
+
+        this.question.html("How many time zones are there in Russia? ")
+        this.question.position(150, 80);
+        this.option1.html("1: 13 ")
+        this.option1.position(150,100)
+        this.option2.html("2: 11")
+        
+        this.input1.position(150,230)
+    }
+  }
+  
